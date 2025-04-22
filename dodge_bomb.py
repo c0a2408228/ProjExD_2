44607c10a85a82755699cb297ebca446f1ab8511
c0a2408228 <c0a2408228@edu.teu.ac.jp>
2025-04-22 14:59:24 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -14,6 +14,21 @@ DELTA = {
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+def check_bound(rct: pg.Rect) -> tuple[bool,bool]:
+    """
+    引数：こうかとんRectまたは爆弾Rect
+    戻り値 : 判定結果タプル （縦,横) 
+    画面内ならTrue,画面外ならFalse
+
+    """
+    yoko, tate = True, True #横、縦方向の変数
+    #横方向判定
+    if rct.left < 0 or WIDTH < rct.right: #画面内だったら
+        yoko = False
+    if rct.top < 0  or HEIGHT < rct.bottom: #画面内だったら
+        tate = False
+    return yoko, tate
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -26,6 +41,7 @@ def main():
     kk_rct.center = 300, 200
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
+    vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -33,6 +49,9 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+        if kk_rct.colliderect(bb_rct):
+            print("Game Over")
+            return
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
@@ -40,8 +59,7 @@ def main():
             if key_lst[key]:
                 sum_mv[0] += mv[0] #左右方向
                 sum_mv[1] += mv[1] #上下方向
-        vx = 5
-        vy = 5
+        
         # if key_lst[pg.K_UP]:
         #     sum_mv[1] -= 5
         # if key_lst[pg.K_DOWN]:
@@ -51,8 +69,17 @@ def main():
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
+        
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx,vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko: #左右どちらかにはみ出ていたら
+            vx *= -1
+        if not tate:
+            vy *= -1
+
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
